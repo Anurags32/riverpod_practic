d09d13936b4971f4ca1b0449b42465940fe6ab2f
Anurags32 <anuragtiwaris1172000@gmail.com>
2025-02-08 14:57:51 +0530

    counter app done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpode_project/screen/counterApp/counter_app.dart';
 import 'package:riverpode_project/screen/home_screen.dart';
 
 void main() {
@@ -12,12 +13,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ProviderScope(
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const HomeScreen(),
+        home: const CounterAppScreen(),
       ),
     );
   }
---
+++ lib/screen/counterApp/counter_app.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final counter = StateProvider<int>((ref){
+  return 0;
+});
+class CounterAppScreen extends ConsumerWidget {
+  const CounterAppScreen({super.key});
+
+  @override
+  Widget build(BuildContext context,WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Counter App"),
+      ),
+      body: Column(
+        children: [
+          Consumer(
+            
+            builder: (context, ref, child) {
+              final counterr = ref.watch(counter);
+              return Center(
+                child: Text(counterr.toString()),
+              );
+            },
+          ),
+          Center(child: ElevatedButton(onPressed: (){
+ref.read(counter.notifier).state++;
+          }, child: Text("+")))
+        ],
+      ),
+    );
+  }
+}
